6071d9698603f5e4e9bf83e0560a89e712f6ad67
Furkan Vural <furkanvural52@gmail.com>
2023-04-02 01:09:23 +0300

    Configure UI elements and assign its constraints, color, temporary definition etc.

--- Stat-ion/Stat-ion/OnboardingVC.swift
+++ Stat-ion/Stat-ion/OnboardingVC.swift
@@ -33,8 +33,40 @@ class OnboardingVC: UIViewController {
     }
     
     private func configureOnboardingView(){
+        
+        let cornerRadius: CGFloat = 20
+        let fontSize: CGFloat = 25
+        let labelColor: UIColor = .white
+        let temporaryText = "Lorem Ipsum Der Ao"
+        
+        //MARK: - ImageView Config
         self.onboardingImageView.image = UIImage(named: "Onboarding")
         self.onboardingImageView.contentMode = .scaleToFill
+        
+        
+        //MARK: - View Config
+        self.mainView.layer.cornerRadius = cornerRadius
+        
+        
+        //MARK: - MapView Config
+        self.mapView.layer.cornerRadius = cornerRadius
+        
+        //MARK: - Labels Config
+        self.firstLabel.textColor = labelColor
+        self.firstLabel.font = .boldSystemFont(ofSize: fontSize)
+        self.firstLabel.text = temporaryText
+        self.secondLabel.textColor = labelColor
+        self.secondLabel.font = .boldSystemFont(ofSize: fontSize)
+        self.secondLabel.text = temporaryText
+        self.thirdLabel.textColor = labelColor
+        self.thirdLabel.font = .boldSystemFont(ofSize: fontSize)
+        self.thirdLabel.text = temporaryText
+        
+        //MARK: - StartButton Config
+        self.startButton.tintColor = .black
+        self.startButton.setTitle("Başla", for: .normal)
+        
+        
     }
     
     
