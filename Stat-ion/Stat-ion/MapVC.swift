acb1ef44c687fd89f411e4c28f71a6c3eb84577b
Furkan Vural <furkanvural52@gmail.com>
2023-04-03 01:05:38 +0300

    Connected stationMapView to MapVC and configure map delegate

--- Stat-ion/Stat-ion/MapVC.swift
+++ Stat-ion/Stat-ion/MapVC.swift
@@ -6,14 +6,22 @@
 //
 
 import UIKit
+import CoreLocation
+import MapKit
 
 class MapVC: UIViewController {
 
+    @IBOutlet weak var stationMapView: MKMapView!
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        stationMapView.delegate = self
         
     }
 
+}
+
+extension MapVC: MKMapViewDelegate{
+    
 }
