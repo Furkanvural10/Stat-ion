6ec50caa0685f0202c7e7683922ead5d812ad848
Furkan Vural <furkanvural52@gmail.com>
2023-04-02 15:40:21 +0300

    Added function which get user current location and ask for permission for locations

--- Stat-ion/Stat-ion/OnboardingVC.swift
+++ Stat-ion/Stat-ion/OnboardingVC.swift
@@ -7,29 +7,24 @@
 
 import UIKit
 import MapKit
+import CoreLocation
 
 class OnboardingVC: UIViewController {
 
     @IBOutlet weak var onboardingImageView: UIImageView!
-    
-
     @IBOutlet weak var mainView: UIView!
-    
     @IBOutlet weak var firstLabel: UILabel!
-    
     @IBOutlet weak var secondLabel: UILabel!
-    
     @IBOutlet weak var thirdLabel: UILabel!
-    
-    
     @IBOutlet weak var mapView: MKMapView!
-    
-    
     @IBOutlet weak var startButton: UIButton!
+    
+    var locationManager = CLLocationManager()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         configureOnboardingView()
+        configureMapView()
     }
     
     private func configureOnboardingView(){
@@ -39,9 +34,15 @@ class OnboardingVC: UIViewController {
         let labelColor: UIColor = .white
         let temporaryText = "Lorem Ipsum Der Ao"
         
+        
+        
         //MARK: - ImageView Config
         self.onboardingImageView.image = UIImage(named: "Onboarding")
         self.onboardingImageView.contentMode = .scaleToFill
+        let overlay = UIView(frame: onboardingImageView.bounds)
+        overlay.backgroundColor = UIColor.black
+        overlay.alpha = 0.4
+        onboardingImageView.addSubview(overlay)
         
         
         //MARK: - View Config
@@ -66,19 +67,33 @@ class OnboardingVC: UIViewController {
         self.startButton.tintColor = .black
         self.startButton.setTitle("Başla", for: .normal)
         
-        let overlay = UIView(frame: onboardingImageView.bounds)
-        overlay.backgroundColor = UIColor.black
-        overlay.alpha = 0.4
-        onboardingImageView.addSubview(overlay)
-        
-        
+    
+    }
+    private func configureMapView(){
+        self.locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyKilometer
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
     }
     
     
     @IBAction func startButton(_ sender: Any) {
     }
     
-    
+}
 
+extension OnboardingVC: CLLocationManagerDelegate{
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        let latitude = locations[0].coordinate.latitude
+        let longitude = locations[0].coordinate.longitude
+        let spanLatitudeDelta = 0.1
+        let spanLongitudeDelta = 0.1
+        let location = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+        let span = MKCoordinateSpan(latitudeDelta: spanLatitudeDelta, longitudeDelta: spanLongitudeDelta)
+        let region = MKCoordinateRegion(center: location, span: span)
+        self.mapView.setRegion(region, animated: true)
+    }
+    
 }
 
