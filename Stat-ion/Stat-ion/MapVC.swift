1ea1b48f0839fe8c134634bb4a06bcaf4183c9af
Furkan Vural <furkanvural52@gmail.com>
2023-04-02 16:16:26 +0300

    Added MapVC and relevant viewcontroller

---
+++ Stat-ion/Stat-ion/MapVC.swift
@@ -0,0 +1,18 @@
+//
+//  MapVC.swift
+//  Stat-ion
+//
+//  Created by furkan vural on 2.04.2023.
+//
+
+import UIKit
+
+class MapVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
