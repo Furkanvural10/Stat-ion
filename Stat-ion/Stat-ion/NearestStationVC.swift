4324290103121018cbf230a6ac08a531d1d5118b
Furkan Vural <furkanvural52@gmail.com>
2023-04-05 01:59:49 +0300

    Added UI elements and connect to NearestStationVC

--- Stat-ion/Stat-ion/NearestStationVC.swift
+++ Stat-ion/Stat-ion/NearestStationVC.swift
@@ -8,22 +8,18 @@
 import UIKit
 
 class NearestStationVC: UIViewController {
-
+    
+    
+    @IBOutlet weak var nearestStationPageTitle: UINavigationBar!
+    
+    @IBOutlet weak var nearestStationTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
