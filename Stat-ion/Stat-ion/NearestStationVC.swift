72893b7ff4d95a9be225bfa5f67d387385f11a45
Furkan Vural <furkanvural52@gmail.com>
2023-04-05 02:10:04 +0300

    Configure UI elements and assign its constraints and tableView connected to vc

--- Stat-ion/Stat-ion/NearestStationVC.swift
+++ Stat-ion/Stat-ion/NearestStationVC.swift
@@ -16,10 +16,32 @@ class NearestStationVC: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        
+        overrideUserInterfaceStyle = .light
+        configurationView()
+    }
+    
+    private func configurationView(){
+        nearestStationTableView.delegate = self
+        nearestStationTableView.dataSource = self
     }
     
 
 
+}
+
+extension NearestStationVC: UITableViewDelegate, UITableViewDataSource{
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        var content = cell.defaultContentConfiguration()
+        content.text = "Deneme"
+        cell.contentConfiguration = content
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    
 }
