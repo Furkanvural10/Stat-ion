dd91a7a0e51bb709ec6bf6d652af7960f30877d3
Furkan Vural <furkanvural52@gmail.com>
2023-04-04 18:52:24 +0300

    Added function which filter the nearest stations users around and added ellipse image to assets folder

--- Stat-ion/Stat-ion/MapVC.swift
+++ Stat-ion/Stat-ion/MapVC.swift
@@ -19,7 +19,10 @@ class MapVC: UIViewController, MKMapViewDelegate {
     var latitude: Double?
     var longitude: Double?
     var annotations = [MKPointAnnotation]()
+    var userLocation: CLLocation?
     @IBOutlet weak var stationFabIcon: UIButton!
+    var station: Station?
+    var stationList = [Station]()
     
     
     @IBOutlet weak var showCurrentLocationButton: UIButton!
@@ -54,13 +57,21 @@ class MapVC: UIViewController, MKMapViewDelegate {
             if error == nil {
                 
                 for document in querySnapshot!.documents{
+                    
                     let geopoint = document.get("coordinate") as! GeoPoint
-                    let title = document.get("stationName") as! String
+                    let stationName = document.get("stationName") as! String
+                    let stationType = document.get("stationType") as! String
+                    let soket1 = document.get("soket1") as! String
+                    let soket2 = document.get("soket2") as! String
+                    
+                    self.station = Station(stationName: stationName, stationType: stationType, soket1: soket1, soket2: soket2, geopoint: geopoint)
+                    self.stationList.append(self.station!)
+                    
                     let coordinate = CLLocationCoordinate2D(latitude: geopoint.latitude, longitude: geopoint.longitude)
                     let annotation = MKPointAnnotation()
                     annotation.coordinate = coordinate
                     self.annotations.append(annotation)
-                    annotation.title = title
+                    annotation.title = stationName
                     self.stationMapView.addAnnotation(annotation)
                 }
                 
@@ -72,13 +83,13 @@ class MapVC: UIViewController, MKMapViewDelegate {
         navigationItem.hidesBackButton = true
         self.showCurrentLocationButton.setImage(UIImage(systemName: "location.fill"), for: .normal)
         self.showCurrentLocationButton.layer.cornerRadius = self.showCurrentLocationButton.frame.width / 2
-        self.showCurrentLocationButton.backgroundColor = .black
-        self.showCurrentLocationButton.tintColor = .white
+        self.showCurrentLocationButton.backgroundColor = .white
+        self.showCurrentLocationButton.tintColor = .systemBlue
         
-        self.stationFabIcon.setImage(UIImage(systemName: "battery.100.bolt"), for: .normal)
+        self.stationFabIcon.setImage(UIImage(named: "Station"), for: .normal)
         self.stationFabIcon.layer.cornerRadius = self.stationFabIcon.frame.width / 2
-        self.stationFabIcon.backgroundColor = .black
-        self.stationFabIcon.tintColor = .white
+        self.stationFabIcon.backgroundColor = .white
+        
         
         
         //MARK: Map
@@ -113,7 +124,18 @@ class MapVC: UIViewController, MKMapViewDelegate {
     }
     
     @IBAction func showStations(_ sender: Any) {
-        self.stationMapView.showAnnotations(self.annotations, animated: true)
+        let maxDistance: CLLocationDistance = 5000 // in meters
+        let filteredAnnotations = annotations.filter { annotation in
+            let annotationLocation = CLLocation(latitude: annotation.coordinate.latitude, longitude: annotation.coordinate.longitude)
+            let distance = annotationLocation.distance(from: userLocation!)
+            return distance <= maxDistance
+        }
+//        self.stationMapView.showAnnotations(filteredAnnotations, animated: true)
+        for anotation in filteredAnnotations{
+            print(anotation.coordinate.latitude)
+            
+        }
+        
     }
     
    
@@ -132,11 +154,20 @@ extension MapVC: CLLocationManagerDelegate{
             let span = MKCoordinateSpan(latitudeDelta: latitudeDelta, longitudeDelta: longitudeDelta)
             let region = MKCoordinateRegion(center: coordinate, span: span)
             self.stationMapView.setRegion(region, animated: true)
+            self.userLocation = locations.last
             locationManager.stopUpdatingLocation()
     }
     
     func mapView(_ mapView: MKMapView, didSelect view: MKAnnotationView) {
         print(view.annotation?.title)
     }
+}
+
+struct Station {
+    var stationName: String
+    var stationType: String
+    var soket1: String
+    var soket2: String
+    var geopoint: GeoPoint
 }
 
