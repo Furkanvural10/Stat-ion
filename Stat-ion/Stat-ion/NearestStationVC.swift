b751040e7c37caeb9b40215e58e46503cacb515d
Furkan Vural <furkanvural52@gmail.com>
2023-04-05 17:15:23 +0300

    Added function which zip two arrays([station], [distance]), and sorted by distance

--- Stat-ion/Stat-ion/NearestStationVC.swift
+++ Stat-ion/Stat-ion/NearestStationVC.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import FirebaseFirestore
 
 class NearestStationVC: UIViewController {
     
@@ -14,12 +15,14 @@ class NearestStationVC: UIViewController {
     @IBOutlet weak var nearestStationTableView: UITableView!
     var station: [Station]?
     var distanceKM: [Double]?
+    var nearestStation: [NearestStation]?
 
     
     override func viewDidLoad() {
         super.viewDidLoad()
         overrideUserInterfaceStyle = .light
         configurationView()
+        zipTwoArray()
     }
     
     private func configurationView(){
@@ -27,6 +30,23 @@ class NearestStationVC: UIViewController {
         nearestStationTableView.dataSource = self
         nearestStationPageTitle.topItem?.title = "En Yakın İstasyonlar"
         
+        
+    }
+    
+    private func zipTwoArray(){
+        self.nearestStation = zip(station!, distanceKM!).map { station, distance in
+            return NearestStation(
+                stationName: station.stationName,
+                stationType: station.stationType,
+                soket1: station.soket1,
+                soket2: station.soket2,
+                geopoint: station.geopoint,
+                distance: distance
+            )
+        }
+        self.nearestStation = self.nearestStation!.sorted {
+            $0.distance < $1.distance
+        }
         
     }
     
@@ -39,7 +59,8 @@ extension NearestStationVC: UITableViewDelegate, UITableViewDataSource{
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell()
         var content = cell.defaultContentConfiguration()
-        content.text = "\(station![indexPath.row].stationName) (\(distanceKM![indexPath.row]) Km)"
+        content.text = "\(nearestStation![indexPath.row].stationName) (\(nearestStation![indexPath.row].distance) KM)"
+        
         cell.contentConfiguration = content
         return cell
     }
@@ -49,8 +70,20 @@ extension NearestStationVC: UITableViewDelegate, UITableViewDataSource{
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        print(station![indexPath.row].stationName)
+        print(self.nearestStation![indexPath.row].stationName)
     }
     
     
+    
 }
+
+struct NearestStation {
+    var stationName: String
+    var stationType: String
+    var soket1: String
+    var soket2: String
+    var geopoint: GeoPoint
+    var distance: Double
+}
+
+
