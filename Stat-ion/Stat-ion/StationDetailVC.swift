2cafdf4c6876741e6024b74cea4bd5954ba80542
Furkan Vural <furkanvural52@gmail.com>
2023-04-06 03:08:07 +0300

    Added configurationView function which configure added UI elements (color, alpha. font etc.)

--- Stat-ion/Stat-ion/StationDetailVC.swift
+++ Stat-ion/Stat-ion/StationDetailVC.swift
@@ -29,17 +29,41 @@ class StationDetailVC: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        configurationView()
+    }
+    
+    private func configurationView(){
+        self.navigationBar.topItem?.title = "İstasyonun İsmi"
+        self.carItem.image = UIImage(systemName: "car.fill")
+        self.stationTypeLabel.text = "Station Type: N22"
+        self.stationTypeLabel.font = .systemFont(ofSize: 14)
+        self.stationTypeLabel.alpha = 0.8
+        
+        self.distanceLabel.text = "12.4 KM"
+        self.distanceLabel.alpha = 0.8
+        self.distanceLabel.font = .systemFont(ofSize: 14)
+        
+        self.firstView.layer.cornerRadius = 10
+        self.secondView.layer.cornerRadius = 10
+        
+        self.firstSoketLabel.text = "Soket-1"
+        self.firstSoketLabel.textColor = .black
+        self.secondSoketLabel.text = "Soket-2"
+        self.secondSoketLabel.textColor = .black
+        
+        let text = "380 V, 32 A, 3 Faz AC"
+        let replaceText = text.replacingOccurrences(of: ", ", with: "\n•")
+        self.firstSoketType.text = "•\(replaceText)"
+        self.secondSoketType.text = "•\(replaceText)"
+        self.firstSoketType.numberOfLines = 0
+        self.secondSoketType.numberOfLines = 0
+        
+        
+        self.firstSoketType.alpha = 0.9
+        self.secondSoketType.alpha = 0.9
     }
     
 
-    /*
-    // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
