b07ed500f213825ddd91ab3e20d001da7ef07586
Furkan Vural <furkanvural52@gmail.com>
2023-04-08 17:56:14 +0300

    Added showBasicAlert function

---
+++ Stat-ion/Stat-ion/CustomUI/Alert.swift
@@ -0,0 +1,24 @@
+//
+//  Alert.swift
+//  Stat-ion
+//
+//  Created by furkan vural on 8.04.2023.
+//
+
+import Foundation
+import UIKit
+
+struct Alert {
+    
+    private static func showBasicAlert(on vc: UIViewController, with title: String, message: String ){
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let button = UIAlertAction(title: "Ok", style: .default)
+        alert.addAction(button)
+        DispatchQueue.main.async {
+            vc.present(alert, animated: true)
+        }
+    }
+    
+    
+    
+}
